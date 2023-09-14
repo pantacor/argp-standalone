1bd32e7b9d08e73678e0c601904802998799c8c2
Alexander Sack <asac@pantacor.com>
2023-09-14 08:21:18 +0200

    add atom.mk

---
+++ atom.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := argp-standalone
+LOCAL_DESCRIPTION := arg-standalonep
+LOCAL_LIBRARIES :=
+
+LOCAL_EXPORT_LDLIBS =
+
+LOCAL_AUTOTOOLS_VERSION := 2.0.4
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS :=
+LOCAL_AUTOTOOLS_CONFIGURE_ENV :=  
+
+define LOCAL_AUTOTOOLS_CMD_POST_INSTALL
+        cp -vf $(PRIVATE_OBJ_DIR)/libargp.a \
+		$(TARGET_OUT_STAGING)/usr/lib/; \
+	cp -vf $(PRIVATE_SRC_DIR)/argp.h \
+		$(TARGET_OUT_STAGING)/usr/include
+endef
+
+include $(BUILD_AUTOTOOLS)
+
